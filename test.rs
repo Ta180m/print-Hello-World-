54129ba82f3235dc49c7329d7dab0387bcd02a50
Anthony Wang <ta180m@gmail.com>
2021-04-18 21:31:06 -0500

    Create test.rs

---
+++ test.rs
@@ -0,0 +1,25 @@
+fn gcd(a : i32, b : i32) -> i32 {
+    if b > 0 {
+        return gcd(b, a % b);
+    } else {
+        return a;
+    }
+}
+
+fn main() {
+    println!("Hello World");
+    
+    const N: i32 = 1000;
+    let mut A: [i32; N as usize] = [0; N as usize];
+    for i in 0..N {
+        A[i as usize] = i;
+    }
+    
+    let mut ans = 0;
+    for i in &A {
+        for j in &A {
+            ans += gcd(*i, *j);
+        }
+    }
+    println!("{}", ans);
+}
